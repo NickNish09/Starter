855721279763e1cf8e3120ceb01ee5669cfae593
nicknish09 <nnmarques97@gmail.com>
2019-07-03 19:52:12 -0300

    logica de redirecionamento pra admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_admin?
+      admin_index_path
+    else
+      root_path
+    end
+  end
+
+  def after_update_path_for(resource_or_scope)
+    if resource_or_scope.is_admin?
+      admin_index_path
+    else
+      root_path
+    end
+  end
 end
