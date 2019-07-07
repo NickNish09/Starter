d698746a802e5581b94893cdf8a6973d64216a39
nicknish09 <nnmarques97@gmail.com>
2019-07-07 09:07:42 -0300

    datatable atualizado

--- app/javascript/packs/datatable.js
+++
@@ -1,7 +0,0 @@
-require('datatables.net-bs');
-require('datatables.net-buttons-bs');
-require('datatables.net-buttons/js/buttons.colVis.js');
-require('datatables.net-buttons/js/buttons.html5.js');
-require('datatables.net-buttons/js/buttons.print.js');
-require('datatables.net-responsive-bs');
-require('datatables.net-select');
